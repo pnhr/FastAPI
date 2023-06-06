1ee82ec6f239094477c3f59462d94acd012bc98a
phetanshi <padmasekhar.pottepalem@outlook.com>
2023-06-06 22:41:41 +0530

    changed connection string

--- repo/database.py
+++ repo/database.py
@@ -2,10 +2,18 @@ import pyodbc
 
 class Database:
     def connect():
+        # connection = pyodbc.connect(driver = '{ODBC Driver 17 for SQL Server}', 
+        #                             host = 'PADMASEKHAR-NEW', 
+        #                             database = 'EmployeeManagment', 
+        #                             trusted_connection='yes')
+
         connection = pyodbc.connect(driver = '{ODBC Driver 17 for SQL Server}', 
-                                    host = 'PADMASEKHAR-NEW', 
-                                    database = 'EmployeeManagment', 
-                                    trusted_connection='yes')
+                                    host = 'psnaz.database.windows.net', 
+                                    database = 'psn',
+                                    UID='psn',
+                                    PWD='MySelf@123',
+                                    trusted_connection='no')
+        
         return connection
     
     def execute_select(self, query:str):
